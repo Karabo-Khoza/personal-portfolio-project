172493970fce1edf917e3d993a0da7755f49d38a
Karabo-Khoza <laurah.karabo@gmail.com>
2025-09-30 21:30:24 +0200

    Put working links, did my final touches

---
+++ src/index.js
@@ -0,0 +1,59 @@
+document.getElementById("myDropdown").onchange = function () {
+  if (this.value) {
+    window.open(this.value, "_blank");
+  }
+};
+
+document.getElementById("myDropdownOne").onchange = function () {
+  if (this.value) {
+    window.open(this.value, "_blank");
+  }
+};
+
+document.getElementById("myDropdownTwo").onchange = function () {
+  if (this.value) {
+    window.open(this.value, "_blank");
+  }
+};
+
+document.getElementById("myDropdownThree").onchange = function () {
+  if (this.value) {
+    window.open(this.value, "_blank");
+  }
+};
+
+document.getElementById("myDropdownFour").onchange = function () {
+  if (this.value) {
+    window.open(this.value, "_blank");
+  }
+};
+
+document.getElementById("myDropdownFive").onchange = function () {
+  if (this.value) {
+    window.open(this.value, "_blank");
+  }
+};
+
+document.getElementById("myDropdownSix").onchange = function () {
+  if (this.value) {
+    window.open(this.value, "_blank");
+  }
+};
+
+document.getElementById("myDropdownSeven").onchange = function () {
+  if (this.value) {
+    window.open(this.value, "_blank");
+  }
+};
+
+document.getElementById("myDropdownEight").onchange = function () {
+  if (this.value) {
+    window.open(this.value, "_blank");
+  }
+};
+
+document.getElementById("myDropdownNine").onchange = function () {
+  if (this.value) {
+    window.open(this.value, "_blank");
+  }
+};
